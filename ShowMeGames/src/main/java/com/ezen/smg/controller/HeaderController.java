7047309b487b4137d4b23d9f1bfb1ef6f8c38a69
LeeRoa <132882290+LeeRoa@users.noreply.github.com>
2023-07-13 11:43:27 +0900

    헤더 컨트롤러 추가

---
+++ ShowMeGames/src/main/java/com/ezen/smg/controller/HeaderController.java
@@ -0,0 +1,11 @@
+package com.ezen.smg.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Controller
+public class HeaderController {
+	
+}
