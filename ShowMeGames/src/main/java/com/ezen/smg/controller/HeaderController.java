7b975593e43a2c4318f8a1f4d4b0e6d4d40ff1b5
LeeRoa <132882290+LeeRoa@users.noreply.github.com>
2023-07-13 14:07:53 +0900

    index header include 처리

--- ShowMeGames/src/main/java/com/ezen/smg/controller/HeaderController.java
+++ ShowMeGames/src/main/java/com/ezen/smg/controller/HeaderController.java
@@ -1,6 +1,7 @@
 package com.ezen.smg.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.log4j.Log4j;
 
@@ -8,4 +9,9 @@ import lombok.extern.log4j.Log4j;
 @Controller
 public class HeaderController {
 	
+	@GetMapping("/customer/faq")
+	public String faq() {
+		log.info("자주 묻는 질문으로 들어갑니다.");
+		return "customer/faq";
+	}
 }
