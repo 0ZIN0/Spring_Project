0787cf1bae00e1769f6753c8df4f615c02e138ce
rhdqnrhdqn <132882266+rhdqnrhdqn@users.noreply.github.com>
2023-07-13 15:00:52 +0900

    장바구니 추가

--- ShowMeGames/src/main/java/com/ezen/smg/controller/HeaderController.java
+++ ShowMeGames/src/main/java/com/ezen/smg/controller/HeaderController.java
@@ -18,4 +18,11 @@ public class HeaderController {
 	public void games() {
 		log.info("모든 게임을 보는 화면입니다.");
 	}
+	
+	@GetMapping(value="/cart")
+	public void cart() {
+		log.info("장바구니 페이지입니다.");
+	}
+	
+	
 }
