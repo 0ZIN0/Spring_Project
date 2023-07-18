5c2e1b21f681b1e93888760bd5bc5e7a27a7be53
rhdqnrhdqn <132882266+rhdqnrhdqn@users.noreply.github.com>
2023-07-18 09:42:35 +0900

    임시커밋

--- ShowMeGames/src/main/webapp/WEB-INF/views/include/header/header.jsp
+++ ShowMeGames/src/main/webapp/WEB-INF/views/include/header/header.jsp
@@ -3,24 +3,6 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <script src="https://kit.fontawesome.com/c48a5ad62b.js" crossorigin="anonymous"></script>
 <header>
-<<<<<<< HEAD
-	<script src="https://kit.fontawesome.com/e49f385ff0.js" crossorigin="anonymous"></script>
-	<div id="header-top">
-		<div>
-			<ul id="header-top-ul">
-				<li><a id="logo-btn" href="./">로고</a></li>
-				<li><a id="game-btn" href="./">게임</a></li>
-				<li><a id="notices-btn" href="./customer/notice">공지 사항</a></li>
-				<li><a id="customer-btn" href="">고객 지원</a>
-					<ul id="customer-ul">
-						<li><a id="faq-btn" href="./customer/faq">자주 묻는 질문</a></li>
-						<li><a id="qna-btn" href="./customer/qna">문의하기</a></li>
-					</ul></li>
-				<li><a id="login-btn" href="./member/login">로그인</a></li>
-			</ul>
-		</div>
-	</div>
-=======
 
    <div id="header-top">
       <div>
@@ -52,7 +34,6 @@
    		<ul id="header-top-ul-underline2"></ul>
    	</div>
    	
->>>>>>> 7c8ebd84c17aa7bd5b68d35714b428cf4540c69f
 	<div id="header-bottom">
 		<div>
 			<ul id="header-bottom-ul">
@@ -112,6 +93,3 @@
 
 </header>
 
-<script>
-   
-</script>
--- ShowMeGames/src/main/webapp/resources/index/js/header.js
+++ ShowMeGames/src/main/webapp/resources/index/js/header.js
@@ -11,22 +11,22 @@ $("#customer-btn").on("click", (e) => {
 
 // <!-- 상단바 게임버튼, 공지사항버튼 호버기능 -->
 $("#notices-btn").on("mouseover", () => {
-    $("#notices-btn-underline").css("backgroundColor", "white");
+    $("#notices-btn-underline").css("backgroundColor", "red");
     $("#notices-btn-underline").css("transition", "background-color 0.3s ease");
 });
 
 $("#notices-btn").on("mouseout", () => {
-    $("#notices-btn-underline").css("backgroundColor", "");
+    $("#notices-btn-underline").css("backgroundColor", "blue");
     $("#notices-btn-underline").css("transition", "background-color 0.4s ease");
 });
 
 $("#customer-btn").on("mouseover", () => {
-    $("#customer-btn-underline").css("backgroundColor", "white");
+    $("#customer-btn-underline").css("backgroundColor", "red");
     $("#customer-btn-underline").css("transition", "background-color 0.3s ease");
 });
 
 $("#customer-btn").on("mouseout", () => {
-    $("#customer-btn-underline").css("backgroundColor", "");
+    $("#customer-btn-underline").css("backgroundColor", "blue");
     $("#customer-btn-underline").css("transition", "background-color 0.4s ease");
 });
 
