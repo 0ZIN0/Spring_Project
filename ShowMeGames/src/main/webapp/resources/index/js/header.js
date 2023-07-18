ccce44cab69bf6901cc6db3931433e1e0ae1bf15
LeeRoa <132882290+LeeRoa@users.noreply.github.com>
2023-07-18 13:55:54 +0900

    header bottom save

--- ShowMeGames/src/main/webapp/resources/index/js/header.js
+++ ShowMeGames/src/main/webapp/resources/index/js/header.js
@@ -30,41 +30,83 @@ $("#notices-btn").on("mouseout", () => {
 //     $("#customer-btn-underline").css("transition", "background-color 0.4s ease");
 // });
 
+// header-bottom list css active
+function openOthers(list) {
+    var tablinks = $('.sub-ul');
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    list.addClass('active');
+    list.css('display', 'grid');
+}
+
+// header-bottom title css active
+function clickChangeColor(btn, list) {
+    if (btn.hasClass('title-active')) {
+      btn.className = btn.removeClass('title-active');
+      return;
+    } else {
+      var tablinks = $('.title-div');
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" title-active", "");
+      }
+      btn.addClass('title-active');
+      openOthers(list);
+    }
+}
+
 // header-bottom 버튼 기능 구현
-$("#category-btn").on("click", (e) => {
+$("#category").on("click", (e) => {
     e.preventDefault();
-    
+    var category =  $('#category-li');
+    var categoryBtn = $('#category');
+
     $('#category-li').toggle();
     $('#genre-li').hide();
     $('#editor-pink-li').hide();
-    $("#customer-ul").hide();
-    $('#header-genre').css({'background':'white', 'border-radius':'10px'});
-    $('#editor').css({'background':'white', 'border-radius':'10px'});
-    $('#category').css({'background':'#E5F3F6', 'border-radius':'10px'});
+    
+    clickChangeColor(categoryBtn, category);
 });
 
 $("#header-genre").on("click", (e) => {
     e.preventDefault();
-    
+    var genre =  $('#genre-li');
+    var genreBtn = $('#header-genre');
+
     $('#category-li').hide();
     $('#genre-li').toggle();
     $('#editor-pink-li').hide();
-    $("#customer-ul").hide();
-    $('#header-genre').css({'background':'#E5F3F6', 'border-radius':'10px'});
-    $('#category').css({'background':'white', 'border-radius':'10px'});
-    $('#editor').css({'background':'white', 'border-radius':'10px'});
+    
+    clickChangeColor(genreBtn, genre);
 });
 
 $("#editor").on("click", (e) => {
     e.preventDefault();
+    var editor =  $('#editor-pink-li');
+    var editorBtn =  $('#editor');
     
     $('#category-li').hide();
     $('#genre-li').hide();
     $('#editor-pink-li').toggle();
-    $("#customer-ul").hide();
-    $('#editor').css({'background':'#E5F3F6', 'border-radius':'10px'});
-    $('#header-genre').css({'background':'white', 'border-radius':'10px'});
-    $('#category').css({'background':'white', 'border-radius':'10px'});
+
+    clickChangeColor(editorBtn, editor);
+});
+
+$('html').click(function(e) {   
+	if($(e.target).hasClass("sub-ul") || $(e.target).hasClass("title-div")) {
+  } else {
+    console.log("지워");
+    $('#category-li').hide();
+    $('#genre-li').hide();
+    $('#editor-pink-li').hide();
+    var tablinks = $('.title-div');
+    var tablinks2 = $('.sub-ul');
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" title-active", 
+      "");
+      tablinks2[i].className = tablinks2[i].className.replace(" active", "");
+    }
+  }
 });
 
 // header scroll 기능 구현
@@ -79,7 +121,7 @@ $(window).scroll(function () {
 
 }); 
 let lastScroll = 0;
-$(window).on('scroll', function(){
+$(window).on('scroll', function() {
     let scrollTop = $(this).scrollTop();
 
     if (scrollTop <= 200) {
