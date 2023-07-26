336744f36903c2f9c50def4c41f40ce666ebf82c
LeeRoa <132882290+LeeRoa@users.noreply.github.com>
2023-07-26 15:20:24 +0900

    cart service 수정

--- ShowMeGames/src/main/java/com/ezen/smg/controller/HeaderController.java
+++ ShowMeGames/src/main/java/com/ezen/smg/controller/HeaderController.java
@@ -1,7 +1,5 @@
 package com.ezen.smg.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,7 +28,7 @@ public class HeaderController {
 	
 	@GetMapping(value="/cart")
 	public void cart(Model model) {
-		
+		log.info("1");
 //		model.addAttribute("carts", cartService.getCartList());
 	}
 }
