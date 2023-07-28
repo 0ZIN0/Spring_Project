6f89cedf1d5b8d86687a9cb54c2c5c24b9999cb6
VintageGT <114378858+VintageGentleman@users.noreply.github.com>
2023-07-28 15:32:03 +0900

    home 레이아웃

--- ShowMeGames/src/main/java/com/ezen/smg/controller/IndexController.java
+++ ShowMeGames/src/main/java/com/ezen/smg/controller/IndexController.java
@@ -1,19 +1,33 @@
 package com.ezen.smg.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ezen.smg.service.IndexService;
 
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 @Controller
 public class IndexController {
+
+	@Autowired
+	IndexService serv;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
+	public String home(Model model) {
+		
+		model.addAttribute("latestList", serv.getLatestGameList());
+		model.addAttribute("editorList", serv.getEditorRecmdList());
+		model.addAttribute("discountList", serv.getLargestDiscountList());
+		model.addAttribute("curatorList", serv.getCuratorRecmdList());
+		
 		log.info("main 실행");
+		
 		return "index";
 	}
 	
