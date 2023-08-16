d9947de1f4c705247c863b38718e381100d15898
0ZIN0 <112708158+0ZIN0@users.noreply.github.com>
2023-08-16 14:20:38 +0900

    로그인 위치 수정

--- ShowMeGames/src/main/java/com/ezen/smg/common/WebMvcConfig.java
+++ ShowMeGames/src/main/java/com/ezen/smg/common/WebMvcConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.ezen.smg.interceptor.HeaderInterceptor;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
  
