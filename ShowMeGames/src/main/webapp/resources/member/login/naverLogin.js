ebaecf5fb8bfee3f404038616871c9eb4c2a7389
0ZIN0 <112708158+0ZIN0@users.noreply.github.com>
2023-08-29 10:36:36 +0900

    콜백 주소 수정

--- ShowMeGames/src/main/webapp/resources/member/login/naverLogin.js
+++ ShowMeGames/src/main/webapp/resources/member/login/naverLogin.js
@@ -1,16 +1,15 @@
 var naverLogin = new naver.LoginWithNaverId({
-        clientId: "lmZiRViUOnHbdLiKlCPj",
-        // 본인의 Client ID로 수정, 띄어쓰기는 사용하지 마세요.
-        callbackUrl: "http://localhost:8090/smg/member/naver_login",
-        // 본인의 callBack url로 수정하세요.
-        isPopup: false,
-        loginButton: {color: "green", type: 3, height: 30}
-        // 네이버 로그인버튼 디자인 설정. 한번 바꿔보세요:D
+  clientId: "lmZiRViUOnHbdLiKlCPj",
+  // 본인의 Client ID로 수정, 띄어쓰기는 사용하지 마세요.
+  callbackUrl: "http://3.39.6.147:8090/smg/member/naver_login",
+  // 본인의 callBack url로 수정하세요.
+  isPopup: false,
+  loginButton: { color: "green", type: 3, height: 30 },
+  // 네이버 로그인버튼 디자인 설정. 한번 바꿔보세요:D
 });
 naverLogin.init();
 
-$(document).on("click", "#naverLoginBtn", function(){
-    var naverLogin = document.getElementById("naverIdLogin").firstChild;
-    naverLogin.click();
+$(document).on("click", "#naverLoginBtn", function () {
+  var naverLogin = document.getElementById("naverIdLogin").firstChild;
+  naverLogin.click();
 });
-
